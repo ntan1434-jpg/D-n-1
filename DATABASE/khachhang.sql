01b9e7a3b91839cfbe8a2bdf73b5c14ed54175a2
ntan1434-jpg <ntan1434@gmail.com>
2025-11-08 18:04:03 +0700

    khachhang

---
+++ DATABASE/khachhang.sql
@@ -0,0 +1,22 @@
+create table khachhang(
+tenkh nvarchar(100),
+id identity(1.1),
+diachi nvarchar(max),
+gioitinh nvarchar(20),
+email nvarchar(200),
+sdt int
+)
+
+create or alter proc them_kh(
+@tenkh nvarchar(100),
+@id identity(1.1),
+@diachi nvarchar(max),
+@gioitinh nvarchar(20),
+@email nvarchar(200),
+@sdt int
+)
+as 
+begin
+insert into khachhang values (@table,@id,@diachi,@gioitinh,@email,@sdt)
+end
+go
